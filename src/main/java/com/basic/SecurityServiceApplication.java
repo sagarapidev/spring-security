0d24c35723532b01e6f23480b34e339b61b6bbad
sagarapidev <106273002+sagarapidev@users.noreply.github.com>
2022-10-24 00:27:21 +0530

    BCryptPasswordEncoder By Using @Lazy

--- src/main/java/com/basic/SecurityServiceApplication.java
+++ src/main/java/com/basic/SecurityServiceApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan({"com.basic"})
+
 @SpringBootApplication
+@ComponentScan({"com.basic"})
 public class SecurityServiceApplication {
 
 	public static void main(String[] args) {
